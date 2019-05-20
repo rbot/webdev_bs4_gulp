baafec6c4938e0ce64c33cbb594c572a3eb09d62
Rich <rbot@cox.net>
2019-05-20 19:57:39 -0400

    Created new Gulpfile with sass, postcss, autoprefixer and browsersync

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -16,12 +16,28 @@ const gulp = require('gulp'),
 //     .pipe(gulp.dest('./app/temp/styles'));
 // });
 
-gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.scss')
-    .pipe(sass(autoprefixer))
-    .on('error', function(errorInfo) {
-      console.log(errorInfo.toString());
-      this.emit('end');
-    })
-    .pipe(gulp.dest('./app/temp/styles'));
-});
+// gulp.task('styles', function() {
+//   return gulp.src('./app/assets/styles/styles.scss')
+//     .pipe(sass(autoprefixer))
+//     .on('error', function(errorInfo) {
+//       console.log(errorInfo.toString());
+//       this.emit('end');
+//     })
+//     .pipe(gulp.dest('./app/temp/styles'));
+// });
+
+function style() {
+  // find scss file
+  return gulp.src('./app/assets/styles/*.scss')
+  .pipe(postcss([ autoprefixer() ]))
+  // pass through sass compiler
+  .pipe(sass())
+  .on('error', function(errorInfo) {
+    console.log(errorInfo.toString());
+    this.emit('end');
+  })
+  // save location for compiled CSS
+  .pipe(gulp.dest('./app/temp/styles'));
+}
+
+exports.style = style;
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,36 @@
-require('./gulp/tasks/styles');
-require('./gulp/tasks/watch');
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      browserSync = require('browser-sync').create();
+
+function style() {
+  // find scss file
+  return gulp.src('./app/assets/styles/*.scss')
+  .pipe(postcss([ autoprefixer() ]))
+  // pass through sass compiler
+  .pipe(sass().on('error', sass.logError))
+  // save location for compiled CSS
+  .pipe(gulp.dest('./app/temp/styles'))
+
+  // browsersync
+  .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './app'
+    }
+  });
+
+  gulp.watch('./app/assets/styles/*.scss', style);
+  gulp.watch('./app/*.html').on('change', browserSync.reload);
+  gulp.watch('./app/assets/scripts/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
     
 
 
